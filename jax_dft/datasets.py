2d21a87b2aedfabe13a40d4a5d7eda5415904992
Ryan Pederson <vgnc@vt.edu>
2021-02-15 12:46:18 -0800

    datasets.py consistency updates

--- jax_dft/datasets.py
+++ jax_dft/datasets.py
@@ -23,30 +23,25 @@ import numpy as np
 import glob
 
 from jax_dft import scf
-
+from jax_dft import utils
 
 # pytype: disable=attribute-error
 
 
-_TEST_DISTANCE_X100 = {
-    'h2_plus': set([
-        64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 248, 256,
-        264, 272, 288, 304, 320, 336, 352, 368, 384, 400, 416, 432, 448, 464,
-        480, 496, 512, 528, 544, 560, 576, 592, 608, 624, 640, 656, 672, 688,
-        704, 720, 736, 752, 768, 784, 800, 816, 832, 848]),
-    'h2': set([
-        40, 56, 72, 88, 104, 120, 136, 152, 184, 200, 216, 232,
-        248, 264, 280, 312, 328, 344, 360, 376, 392, 408, 424,
-        456, 472, 488, 504, 520, 536, 568, 584, 600]),
-    'h4': set([
-        104, 120, 136, 152, 168, 200, 216, 232, 248, 280, 296,
-        312, 344, 360, 376, 392, 408, 424, 440, 456, 472, 488,
-        520, 536, 552, 568, 584, 600]),
-    'h2_h2': set([
-        16, 48, 80, 112, 144, 176, 208, 240, 272, 304, 336, 368,
-        400, 432, 464, 496, 528, 560, 592, 624, 656, 688, 720, 752,
-        784, 816, 848, 880, 912, 944, 976]),
-}
+_TEST_DISTANCE_X100 = {'h2_plus': set(
+  [64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 248, 256, 264, 272,
+    288, 304, 320, 336, 352, 368, 384, 400, 416, 432, 448, 464, 480, 496, 512,
+    528, 544, 560, 576, 592, 608, 624, 640, 656, 672, 688, 704, 720, 736, 752,
+    768, 784, 800, 816, 832, 848]), 'h2': set(
+  [40, 56, 72, 88, 104, 120, 136, 152, 184, 200, 216, 232, 248, 264, 280, 312,
+    328, 344, 360, 376, 392, 408, 424, 456, 472, 488, 504, 520, 536, 568, 584,
+    600]), 'h4': set(
+  [104, 120, 136, 152, 168, 200, 216, 232, 248, 280, 296, 312, 344, 360, 376,
+    392, 408, 424, 440, 456, 472, 488, 520, 536, 552, 568, 584, 600]),
+  'h2_h2': set(
+    [16, 48, 80, 112, 144, 176, 208, 240, 272, 304, 336, 368, 400, 432, 464,
+      496, 528, 560, 592, 624, 656, 688, 720, 752, 784, 816, 848, 880, 912, 944,
+      976]), }
 
 
 class Dataset(object):
@@ -54,7 +49,8 @@ class Dataset(object):
 
   """
 
-  def __init__(self, path=None, data=None, num_grids=None, name=None):
+  def __init__(self, path=None, data=None, num_grids=None,
+      interaction_fn=utils.exponential_coulomb, name=None):
     """Initializer.
 
     Args:
@@ -75,6 +71,8 @@ class Dataset(object):
           external potential. If None, the original grids size are used.
           Otherwise, the original grids are trimmed into num_grids grid points
           in the center.
+      interaction_fn: function takes displacements and returns
+          float numpy array with the same shape of displacements.
       name: String, the name of the dataset.
 
     Raises:
@@ -90,8 +88,9 @@ class Dataset(object):
       setattr(self, name, array)
     self.data = data
     self._set_num_grids(num_grids)
+    self._check_num_electrons_consistency()
+    self._check_external_potential_consistency(interaction_fn)
     self.total_num_samples = self.total_energies.shape[0]
-
 
   def _load_from_path(self, path):
     """Loads npy files from path."""
@@ -129,10 +128,9 @@ class Dataset(object):
       logging.info('This dataset has %d grids.', self.num_grids)
     else:
       if num_grids > original_num_grids:
-        raise ValueError(
-            'num_grids (%d) cannot be '
-            'greater than the original number of grids (%d).'
-            % (num_grids, original_num_grids))
+        raise ValueError('num_grids (%d) cannot be '
+                         'greater than the original number of grids (%d).' % (
+                           num_grids, original_num_grids))
       self.num_grids = num_grids
       diff = original_num_grids - num_grids
       if diff % 2:
@@ -142,14 +140,40 @@ class Dataset(object):
         left_grids_removed = diff // 2
         right_grids_removed = diff // 2
       self.grids = self.grids[
-          left_grids_removed: original_num_grids - right_grids_removed]
-      self.densities = self.densities[
-          :, left_grids_removed: original_num_grids - right_grids_removed]
-      self.external_potentials = self.external_potentials[
-          :, left_grids_removed: original_num_grids - right_grids_removed]
+      left_grids_removed: original_num_grids - right_grids_removed]
+      self.densities = self.densities[:,
+      left_grids_removed: original_num_grids - right_grids_removed]
+      self.external_potentials = self.external_potentials[:,
+      left_grids_removed: original_num_grids - right_grids_removed]
       logging.info(
-          'The original number of grids (%d) are trimmed into %d grids.',
-          original_num_grids, self.num_grids)
+        'The original number of grids (%d) are trimmed into %d grids.',
+        original_num_grids, self.num_grids)
+
+  def _check_external_potential_consistency(self, interaction_fn):
+    """Checks whether external_potential.npy is consistent with given
+    nuclear_charges.npy and locations.npy file. """
+
+    for external_potential, nuclear_charges, locations in zip(
+        self.external_potentials, self.nuclear_charges, self.locations):
+      if np.allclose(external_potential,
+          utils.get_atomic_chain_potential(self.grids, locations,
+            nuclear_charges, interaction_fn)):
+        pass
+      else:
+        raise ValueError('external_potentials.npy is not consistent with given '
+                         'nuclear_charges.npy and locations.npy')
+    return True
+
+  def _check_num_electrons_consistency(self):
+    """Checks whether num_electrons.npy and densities.npy are consistent."""
+
+    for num_electrons, density in zip(self.num_electrons, self.densities):
+      if np.allclose(num_electrons, np.trapz(density, self.grids)):
+        pass
+      else:
+        raise ValueError('num_electrons.npy is not consistent with given '
+                         'densities.npy')
+    return True
 
   def get_mask(self, selected_distance_x100=None):
     """Gets mask from distance_x100."""
@@ -157,13 +181,12 @@ class Dataset(object):
       mask = np.ones(self.total_num_samples, dtype=bool)
     else:
       selected_distance_x100 = set(selected_distance_x100)
-      mask = np.array([
-          distance in selected_distance_x100
-          for distance in self.distances_x100])
+      mask = np.array([distance in selected_distance_x100 for distance in
+        self.distances_x100])
       if len(selected_distance_x100) != np.sum(mask):
         raise ValueError(
-            'selected_distance_x100 contains distance that is not in the '
-            'dataset.')
+          'selected_distance_x100 contains distance that is not in the '
+          'dataset.')
     return mask
 
   def get_test_mask(self):
@@ -176,28 +199,24 @@ class Dataset(object):
     num_samples = np.sum(mask)
 
     if hasattr(self, 'num_unpaired_electrons'):
-        num_unpaired_electrons = self.num_unpaired_electrons[mask]
+      num_unpaired_electrons = self.num_unpaired_electrons[mask]
     else:
-        num_unpaired_electrons = np.repeat(None, repeats=num_samples)
+      num_unpaired_electrons = np.repeat(None, repeats=num_samples)
 
     if hasattr(self, 'spin_densities'):
-        spin_densities = self.spin_densities[mask]
+      spin_densities = self.spin_densities[mask]
     else:
-        spin_densities = np.repeat(None, repeats=num_samples)
+      spin_densities = np.repeat(None, repeats=num_samples)
 
-    return scf.KohnShamState(
-        density=self.densities[mask],
-        spin_density=spin_densities,
-        total_energy=self.total_energies[mask],
-        locations=self.locations[mask],
-        nuclear_charges=self.nuclear_charges[mask],
-        external_potential=self.external_potentials[mask],
-        grids=np.tile(
-            np.expand_dims(self.grids, axis=0), reps=(num_samples, 1)),
-        num_electrons=np.repeat(self.num_electrons, repeats=num_samples),
-        num_unpaired_electrons=num_unpaired_electrons,
-        converged=np.repeat(True, repeats=num_samples),
-        )
+    return scf.KohnShamState(density=self.densities[mask],
+      spin_density=spin_densities, total_energy=self.total_energies[mask],
+      locations=self.locations[mask],
+      nuclear_charges=self.nuclear_charges[mask],
+      external_potential=self.external_potentials[mask],
+      grids=np.tile(np.expand_dims(self.grids, axis=0), reps=(num_samples, 1)),
+      num_electrons=np.repeat(self.num_electrons, repeats=num_samples),
+      num_unpaired_electrons=num_unpaired_electrons,
+      converged=np.repeat(True, repeats=num_samples), )
 
   def get_subdataset(self, mask, exceptions={'grids'}, downsample_step=None):
     """Gets subdataset."""
@@ -222,10 +241,9 @@ class Dataset(object):
       mask = np.ones(self.total_num_samples, dtype=bool)
     else:
       selected_ions = set(selected_ions)
-      mask = np.array([
-        (nuclear_charge[0], num_electron) in selected_ions
-        for (nuclear_charge, num_electron) in zip(self.nuclear_charges,
-                                                  self.num_electrons)])
+      mask = np.array([(nuclear_charge[0], num_electron) in selected_ions for
+        (nuclear_charge, num_electron) in
+        zip(self.nuclear_charges, self.num_electrons)])
       if len(selected_ions) != np.sum(mask):
         raise ValueError(
           'selected_ions contains (nuclear_charge, num_electron) that is not in'
@@ -248,16 +266,12 @@ class Dataset(object):
     else:
       spin_densities = np.repeat(None, repeats=num_samples)
 
-    return scf.KohnShamState(
-        density=self.densities[mask],
-        spin_density= spin_densities,
-        total_energy=self.total_energies[mask],
-        locations=self.locations[mask],
-        nuclear_charges=self.nuclear_charges[mask],
-        external_potential=self.external_potentials[mask],
-        grids=np.tile(
-            np.expand_dims(self.grids, axis=0), reps=(num_samples, 1)),
-        num_electrons=self.num_electrons[mask],
-        num_unpaired_electrons=num_unpaired_electrons,
-        converged=np.repeat(True, repeats=num_samples),
-        )
+    return scf.KohnShamState(density=self.densities[mask],
+      spin_density=spin_densities, total_energy=self.total_energies[mask],
+      locations=self.locations[mask],
+      nuclear_charges=self.nuclear_charges[mask],
+      external_potential=self.external_potentials[mask],
+      grids=np.tile(np.expand_dims(self.grids, axis=0), reps=(num_samples, 1)),
+      num_electrons=self.num_electrons[mask],
+      num_unpaired_electrons=num_unpaired_electrons,
+      converged=np.repeat(True, repeats=num_samples), )
